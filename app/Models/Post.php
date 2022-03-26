de54b03575d98468d8a3f8bf435f5b2c5f1a3f47
Dodi Yulian <cyrillusdodi@gmail.com>
2022-03-26 15:43:29 +0700

    database

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,71 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-  public $title;
-  public $date;
-  public $body;
-  public $excerpt;
-  public $slug;
-
-  public function __construct($title, $date, $body, $excerpt, $slug)
-  {
-    $this->title = $title;
-    $this->date = $date;
-    $this->body = $body;
-    $this->excerpt = $excerpt;
-    $this->slug = $slug;
-  }
-
-  // find all post
-  public static function all()
-  {
-    /* 
-      found files inside resource path with folder's name posts
-      turn it into collection
-      map it, return parse file with yaml front matter to read metadata
-      and then map it again and return it as a Post object
-    */
-    return cache()->rememberForever('posts.all', function () {
-      return collect(File::files(resource_path('posts')))
-        ->map(function ($files) {
-          return YamlFrontMatter::parseFile($files);
-        })
-        ->map(
-          function ($documents) {
-            return new Post(
-              $documents->title,
-              $documents->date,
-              $documents->body(),
-              $documents->excerpt,
-              $documents->slug
-            );
-          }
-        )->sortByDesc('date');
-    });
-  }
-
-  // find a post by its slug
-  public static function find($slug)
-  {
-    $post = static::all();
-
-    return $post->firstWhere('slug', $slug);
-  }
-
-  public static function findOrFail($slug)
-  {
-    $post = static::find($slug);
-
-    if (!$post) {
-      throw new ModelNotFoundException();
-    }
-
-    return $post;
-  }
+    use HasFactory;
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
     <article>
       @foreach( $posts as $post)
       <h1>
-        <a href="posts/{{ $post->slug }}">
+        <a href="posts/{{ $post->id }}">
           {{ $post->title }}
         </a>
       </h1>
