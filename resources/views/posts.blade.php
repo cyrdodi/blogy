1f9317b899d203013484edffa4a62b2a28119b33
Dodi Yulian <cyrillusdodi@gmail.com>
2022-03-28 15:42:06 +0700

    relationship

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,11 +3,11 @@
     <article>
       @foreach( $posts as $post)
       <h1>
-        <a href="posts/{{ $post->slug }}">
+        <a href="/posts/{{ $post->slug }}">
           {{ $post->title }}
         </a>
       </h1>
-      <a href="">{{ $post->category->name }}</a>
+      <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
       <p>
         {{ $post->excerpt }}
       </p>
