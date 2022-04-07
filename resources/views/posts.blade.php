83cc1ed91f662030f55bcd16b31e6f36e40dc7a6
Dodi Yulian <cyrdodi@pop-os.localdomain>
2022-04-07 15:11:17 +0700

    add html style

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,39 @@
 <x-layout>
-  <x-slot name="content">
-    <article>
-      @foreach( $posts as $post)
-      <h1>
-        <a href="/posts/{{ $post->slug }}">
-          {{ $post->title }}
-        </a>
-      </h1>
-      <div>
-        Posted by <a href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> on <a
-          href="/categories/{{ $post->category->slug }}">{{
-          $post->category->name
-          }}</a>
-      </div>
-      <p>
-        {{ $post->excerpt }}
-      </p>
-      @endforeach
-    </article>
-  </x-slot>
+
+
+
+  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-feature-card />
+
+    <div class="lg:grid lg:grid-cols-2">
+      <x-post-card />
+      <x-post-card />
+    </div>
+
+    <div class="lg:grid lg:grid-cols-3">
+      <x-post-card />
+      <x-post-card />
+      <x-post-card />
+
+    </div>
+  </main>
+
+  {{-- <article>
+    @foreach( $posts as $post)
+    <h1>
+      <a href="/posts/{{ $post->slug }}">
+        {{ $post->title }}
+      </a>
+    </h1>
+    <div>
+      Posted by <a href="/author/{{ $post->author->username }}">{{ $post->author->name }}</a> on <a
+        href="/categories/{{ $post->category->slug }}">{{
+        $post->category->name
+        }}</a>
+    </div>
+    <p>
+      {{ $post->excerpt }}
+    </p>
+    @endforeach
+  </article> --}}
 </x-layout>
