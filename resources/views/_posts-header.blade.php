78f538eab3e4d61161537abe2a93a30ed3d6912b
Dodi Yulian <cyrdodi@pop-os.localdomain>
2022-04-07 21:56:59 +0700

    dropdown w/ alpinejs

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -13,23 +13,24 @@
 
   <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
     <!--  Category -->
-    <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-      <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-        <option value="category" disabled selected>Category
-        </option>
-        <option value="personal">Personal</option>
-        <option value="business">Business</option>
-      </select>
-
-      <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22" height="22"
-        viewBox="0 0 22 22">
-        <g fill="none" fill-rule="evenodd">
-          <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-          </path>
-          <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
-        </g>
-      </svg>
+    <div class="relative flex lg:inline-flex items-center lg:w-44  bg-gray-100 rounded-xl">
+      <div x-data="{open:false}" class="w-full">
+        <button @click="open=!open" class="py-2 pl-3 w-full  text-left pr-9 text-sm font-semibold">{{
+          isset($currentCategory) ? $currentCategory->name : 'Category' }}</button>
+        <div x-show="open" @click.outside="open = false"
+          class="absolute z-50 bg-gray-100 mt-2 rounded-xl w-full text-left" style="display:none">
+          <a href="/" class="block px-2 py-1 text-leading-1 text-sm hover:bg-blue-500 hover:text-white">All</a>
+          @foreach($categories as $category)
+          <a href="/categories/{{ $category->slug }}" class="block px-2 py-1 text-leading-1 text-sm hover:bg-blue-500 hover:text-white
+            {{-- $currentCategory->slug === $category->slug same as $currentCategory->is($category) --}}
+            {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}">{{
+            $category->name }}</a>
+          @endforeach
+        </div>
+      </div>
     </div>
+
+
 
     <!-- Other Filters -->
     <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
